d233bd1afaab62744b0684e6e59f31bb896cb440
Chuck D'Antonio <c_dantonio@acm.org>
2016-11-30 14:10:24 -0500

    Parameterized the script and added Gemfire and Concourse

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 
 . lib/env.sh
 . personal.sh
+. lib/setup.sh
 . lib/login_ops_manager.sh
 . lib/eula.sh
 . lib/download_product.sh
@@ -10,13 +11,104 @@
 . lib/stage_product.sh
 . lib/product_guid.sh
 
+parse_args () {
+  if [ $# -eq 0 ] ; then
+    set_defaults
+  else
+    while [ $# -gt 1 ] ; do
+      product=$1
+      case $product in
+          "pcf")
+            INSTALL_PCF=1
+            ;;
+          "mysql")
+            INSTALL_MYSQL=1
+            ;;
+          "rabbit")
+            INSTALL_RABBIT=1
+            ;;
+          "redis")
+            INSTALL_REDIS=1
+            ;;
+          "scs")
+            INSTALL_SCS=1
+            ;;
+          "gcp")
+            INSTALL_GCP=1
+            ;;
+          "gemfire")
+            INSTALL_GEMFIRE=1
+            ;;
+          "default")
+            set_defaults
+            ;;
+          "all")
+            INSTALL_PCF=1
+            INSTALL_MYSQL=1
+            INSTALL_RABBIT=1
+            INSTALL_REDIS=1
+            INSTALL_SCS=1
+            INSTALL_GCP=1
+            INSTALL_GEMFIRE=1
+            INSTALL_CONCOURSE=1
+            ;;
+          *)
+            usage
+            ;;
+      esac
+      shift
+    done
+  fi
+
+}
+
+set_defaults () {
+  INSTALL_PCF=1
+  INSTALL_MYSQL=1
+  INSTALL_RABBIT=1
+  INSTALL_REDIS=1
+  INSTALL_SCS=1
+  INSTALL_GCP=1
+}
+
+usage () {
+  cmd=`basename $0`
+  echo "$cmd [ pcf ] [ mysql ] [ rabbit ] [ redis ] [ scs ] [ gcp ] [ gemfire ] [ concourse ]"
+}
+
 products () {
-  cloud_foundry
-  mysql
-  rabbit
-  redis
-  spring_cloud_services
-  service_broker
+  if [ "$INSTALL_PCF" -eq 1 ] ; then
+    cloud_foundry
+  fi
+
+  if [ "$INSTALL_MYSQL" -eq 1 ] ; then
+    mysql
+  fi
+
+  if [ "$INSTALL_RABBIT" -eq 1 ] ; then
+    rabbit
+  fi
+
+  if [ "$INSTALL_REDIS" -eq 1 ] ; then
+    redis
+  fi
+
+  if [ "$INSTALL_SCS" -eq 1 ] ; then
+    spring_cloud_services
+  fi
+
+  if [ "$INSTALL_GCP" -eq 1 ] ; then
+    service_broker
+  fi
+
+  if [ "$INSTALL_GEMFIRE" -eq 1 ] ; then
+    gemfire
+  fi
+
+  if [ "$INSTALL_CONCOURSE" -eq 1 ] ; then
+    concourse
+  fi
+
 }
 
 cloud_foundry () {
@@ -27,7 +119,7 @@ cloud_foundry () {
   echo "Downloading Cloud Foundry Elastic Runtime..."
   tile_file=`download_product "elastic-runtime" $PCF_VERSION`
   echo "Uploading Cloud Foundry Elastic Runtime..."
-  upload_product $tile_file
+  upload_product $TILE_FILE
   echo "Staging Cloud Foundry Elastic Runtime..."
   stage_product "cf"
   PCF_GUID=`product_guid "cf"`
@@ -63,7 +155,7 @@ mysql () {
   echo "Downloading MySQL Service..."
   tile_file=`download_product "p-mysql" $MYSQL_VERSION`
   echo "Uploading MySQL Service..."
-  upload_product $tile_file
+  upload_product $TILE_FILE
   echo "Staging MySQL Service..."
   stage_product "p-mysql"
   MYSQL_GUID=`product_guid "p-mysql"`
@@ -74,7 +166,7 @@ rabbit () {
   echo "Downloading Rabbit MQ Service..."
   tile_file=`download_product "pivotal-rabbitmq-service" $RABBIT_VERSION`
   echo "Uploading Rabbit MQ Service..."
-  upload_product $tile_file
+  upload_product $TILE_FILE
   echo "Staging Rabbit MQ Service..."
   stage_product "p-rabbitmq"
   RABBIT_GUID=`product_guid "p-rabbitmq"`
@@ -85,18 +177,18 @@ redis () {
   echo "Downloading REDIS Service..."
   tile_file=`download_product "pivotal-rabbitmq-service" $REDIS_VERSION`
   echo "Uploading REDIS Service..."
-  upload_product $tile_file
+  upload_product $TILE_FILE
   echo "Staging REDIS Service..."
   stage_product "p-redis"
   REDIS_GUID=`product_guid "p-redis"`
 }
 
 spring_cloud_services () {
-  accept_eula "p-redis" $SCS_VERSION "yes"
+  accept_eula "p-spring-cloud-services" $SCS_VERSION "yes"
   echo "Downloading Spring Cloud Services..."
   tile_file=`download_product "p-spring-cloud-services" $SCS_VERSION`
   echo "Uploading Spring Cloud Services..."
-  upload_product $tile_file
+  upload_product $TILE_FILE
   echo "Staging Spring Cloud Services..."
   stage_product "p-spring-cloud-services"
   SCS_GUID=`product_guid "p-spring-cloud-services"`
@@ -134,13 +226,36 @@ SQL
   echo "Downloading GCP Service Broker..."
   tile_file=`download_product "p-spring-cloud-services" $GCP_VERSION`
   echo "Uploading GCP Service Broker..."
-  upload_product $tile_file
+  upload_product $TILE_FILE
   echo "Staging GCP Service Broker..."
   stage_product "gcp-service-broker"
   GCP_GUID=`product_guid "gcp-service-broker"`
 }
 
+gemfire () {
+  accept_eula "p-gemfire" $GEM_VERSION "yes"
+  echo "Downloading Gemfire..."
+  tile_file=`download_product "p-gemfire" $GEM_VERSION`
+  echo "Uploading Gemfire..."
+  upload_product $TILE_FILE
+  echo "Staging Gemfire..."
+  stage_product "p-gemfire"
+  GEM_GUID=`product_guid "p-gemfire"`
+}
+
+concourse () {
+  accept_eula "p-concourse" $CONCOURSE_VERSION "yes"
+  echo "Downloading Concourse..."
+  tile_file=`download_product "p-concourse" $CONCOURSE_VERSION`
+  echo "Uploading Concourse..."
+  upload_product $TILE_FILE
+  echo "Staging Concourse..."
+  stage_product "p-concourse"
+  CONCOURSE_GUID=`product_guid "p-concourse"`
+}
+
 env
 setup
-login_ops_manager
-products
+parse_args $@
+# login_ops_manager
+# products
