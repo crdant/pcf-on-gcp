ffe4206426af8fa728ac012286ab633935d7421c
Chuck D'Antonio <c_dantonio@acm.org>
2016-11-30 10:00:40 -0500

    Synced up via copy/paste with other scripts

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 export ACCOUNT="cdantonio@pivotal.io"
-exportPROJECT="fe-cdantonio"
-exportDOMAIN=crdant.io
+export PROJECT="fe-cdantonio"
+export DOMAIN=crdant.io
 
 export REGION_1="us-east1"
 export AVAILABILITY_ZONE_1="${REGION_1}-b"
@@ -14,3 +14,8 @@ export ALL_INTERNET="0.0.0.0/0"
 export OPS_MANAGER_VERSION="1.8.10"
 export OPS_MANAGER_VERSION_TOKEN=`echo ${OPS_MANAGER_VERSION} | tr . -`
 export PCF_VERSION="1.8.16"
+export MYSQL_VERSION="1.8.0-edge.15"
+export RABBIT_VERSION="1.7.6"
+export REDIS_VERSION="1.6.2"
+export GCP_VERSION="2.0.1"
+export SCS_VERSION="1.3.0"
