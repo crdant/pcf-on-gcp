7ec6bb9c883bc2ad703e379693154bd87ae7d29f
Chuck D'Antonio <c_dantonio@acm.org>
2016-11-30 14:19:04 -0500

    Silenced the script

--- lib/setup.sh
+++ lib/setup.sh
@@ -2,11 +2,11 @@
 
 setup () {
   # make sure our API components up-to-date
-  gcloud components update
+  gcloud components update --quiet --verbosity="critical" --no-user-output-enabled
 
   # log in (parameterize later)
-  gcloud auth login ${ACCOUNT}
-  gcloud config set project ${PROJECT}
-  gcloud config set compute/zone ${AVAILABILITY_ZONE_1}
-  gcloud config set compute/region ${REGION_1}
+  gcloud auth login ${ACCOUNT} --quiet --verbosity="critical" --no-user-output-enabled
+  gcloud config set project ${PROJECT} --quiet --verbosity="critical" --no-user-output-enabled
+  gcloud config set compute/zone ${AVAILABILITY_ZONE_1} --quiet --verbosity="critical" --no-user-output-enabled
+  gcloud config set compute/region ${REGION_1} --quiet --verbosity="critical" --no-user-output-enabled
 }
