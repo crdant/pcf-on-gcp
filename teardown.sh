16f0ae621c36407416685318ea927f84f61988ea
Chuck D'Antonio <c_dantonio@acm.org>
2016-12-01 15:25:23 -0500

    Updated to use the KEYDIR environment variable instead of the string 'keys/' in the path to key files

--- teardown.sh
+++ teardown.sh
@@ -54,7 +54,7 @@ ops_manager () {
   # drop Ops Manager
   gcloud compute --project "${PROJECT}" instances delete "pcf-ops-manager-${OPS_MANAGER_VERSION_TOKEN}-${DOMAIN_TOKEN}" --zone ${AVAILABILITY_ZONE_1} --quiet
   gcloud compute --project "${PROJECT}" images delete "pcf-ops-manager-${OPS_MANAGER_VERSION_TOKEN}" --quiet
-  rm keys/ubuntu-key keys/ubuntu-key.pub
+  rm ${KEYDIR}/ubuntu-key ${KEYDIR}/ubuntu-key.pub
 }
 
 load_balancers () {
@@ -125,7 +125,7 @@ dns () {
 security () {
   # remove VCAP SSH from metadata provided to all boxen, this will not preserve keys that were added in different ways (FIX!)
   gcloud compute --project=${PROJECT} project-info remove-metadata --keys sshKeys --quiet
-  rm keys/vcap-key keys/vcap-key.pub
+  rm ${KEYDIR}/vcap-key ${KEYDIR}/vcap-key.pub
 
   # delete the service account
   gcloud iam service-accounts delete bosh-opsman-${DOMAIN_TOKEN}@${PROJECT}.iam.gserviceaccount.com --quiet
