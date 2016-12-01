151ff952388341711fda81fb844a235e28992f71
Chuck D'Antonio <c_dantonio@acm.org>
2016-12-01 16:38:20 -0500

    Debugged conditional logic to properly determine if the user is logged in

--- lib/login_ops_manager.sh
+++ lib/login_ops_manager.sh
@@ -1,8 +1,8 @@
 # log into the operations manager API using UAA
 #    reference $UAA_ACCESS_TOKEN as the bearer token for API calls
 
-valid_login () {
-  status_code=`curl -qs --insecure "https://manager.${SUBDOMAIN}/api/v0/uaa/tokens_expiration"  -H "Authorization: Bearer ${UAA_ACCESS_TOKEN}" -H "Accept: application/json"  -w "%{http_code}\n"`
+validate_login () {
+  status_code=`curl -qs -I --insecure "https://manager.${SUBDOMAIN}/api/v0/uaa/tokens_expiration"  -H "Authorization: Bearer ${UAA_ACCESS_TOKEN}" -H "Accept: application/json"  -w "%{http_code}\n"`
   if [ "$status_code" = "401" ] ; then
     return 0
   else
@@ -18,7 +18,12 @@ do_login () {
 
 login_ops_manager () {
   UAA_ACCESS_TOKEN=`uaac context | grep "access_token" | sed '1s/^[ \t]*access_token: //'`
-  if [ -n "$UAA_ACCESS_TOKEN" ] || [ -n valid_login ] ; then
+  if [ -z "$UAA_ACCESS_TOKEN" ] ; then
+    do_login
+  fi
+
+  validate_login
+  if [ $? -eq 0 ] ; then
     do_login
   fi
 }
