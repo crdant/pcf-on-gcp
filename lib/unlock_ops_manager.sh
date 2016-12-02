7915698000e0b0fe062aa7f05440ea38e1c0dce9
Chuck D'Antonio <c_dantonio@acm.org>
2016-12-02 16:20:53 -0500

    Added function to unlock operations manager

---
+++ lib/unlock_ops_manager.sh
@@ -0,0 +1,6 @@
+# log unlock the operations manager after restart
+
+unlock_ops_manager () {
+    login_ops_manager > /dev/null
+    curl -q --insecure "https://manager.${SUBDOMAIN}/api/v0/unlock" -X PUT -H "Content-Type: application/json" -d '{"passphrase": "tarn-sough-mote-emerge"}'
+}
