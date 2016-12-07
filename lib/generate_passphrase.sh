577314324880c786e20d1e2b2d4263d5b721e8e0
Chuck D'Antonio <c_dantonio@acm.org>
2016-12-07 11:47:10 -0500

    Added function for generating passphrases. Not used in the mainline scripts yet but I'm experimenting with it in personal.sh

---
+++ lib/generate_passphrase.sh
@@ -0,0 +1,42 @@
+# get a random adjective-noun phrase using the same source files as random routes in CF
+#    outputs the phrase
+
+CF_CLI_GITHUB_RAW_ROOT="https://raw.githubusercontent.com/cloudfoundry/cli/master"
+adjective_file="$TMPDIR/adjectives.txt"
+noun_file="$TMPDIR/nouns.txt"
+delimiter="-"
+
+if [ ! -f "${adjective_file}" ] ; then
+  curl -qsf -o "${adjective_file}" "${CF_CLI_GITHUB_RAW_ROOT}/util/words/dict/adjectives.txt"
+fi
+adjective_lines=`wc -l ${adjective_file} | awk '{ print $1 }'`
+
+
+if [ ! -f "${noun_file}" ] ; then
+  curl -qsf -o "${noun_file}" "${CF_CLI_GITHUB_RAW_ROOT}/util/words/dict/nouns.txt"
+fi
+noun_lines=`wc -l ${noun_file} | awk '{ print $1 }'`
+
+generate_passphrase () {
+  words=$1
+
+  counter=0
+  passphrase=""
+  while [ $counter -lt $words ] ; do
+    if [ $((RANDOM%2)) -eq 0 ] ; then
+      file=$adjective_file
+      index=$((RANDOM*RANDOM%$adjective_lines+1));
+    else
+      file=$noun_file
+      index=$((RANDOM*RANDOM%$noun_lines+1));
+    fi
+    word=`sed -n "$index p" "${file}"`
+    passphrase="${passphrase}${word}"
+    let counter=counter+1
+    if [ $counter -lt $words ] ; then
+      passphrase="${passphrase}${delimiter}"
+    fi
+  done
+
+  echo "${passphrase}"
+}
