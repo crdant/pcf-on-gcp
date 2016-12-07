08b398deb52c281a726eb6ba3f49967cd1fd10a2
Chuck D'Antonio <c_dantonio@acm.org>
2016-12-07 08:58:09 -0500

    Added function to get a random phrase like the one in CF for a random route. Used the adjective and noun files from the CF CLI.

---
+++ lib/random_phrase.sh
@@ -0,0 +1,23 @@
+# get a random adjective-noun phrase using the same source files as random routes in CF
+#    outputs the phrase
+
+CF_CLI_GITHUB_RAW_ROOT="https://raw.githubusercontent.com/cloudfoundry/cli/master"
+adjective_file="$TMPDIR/adjectives.txt"
+noun_file="$TMPDIR/nouns.txt"
+
+if [ ! -f "${adjective_file}" ] ; then
+  curl -qsf -o "${adjective_file}" "${CF_CLI_GITHUB_RAW_ROOT}/util/words/dict/adjectives.txt"
+fi
+adjectives_lines=`wc -l $adjective_file | awk '{ print $1 }'`
+
+
+if [ ! -f "${noun_file}" ] ; then
+  curl -qsf -o "${noun_file}" "${CF_CLI_GITHUB_RAW_ROOT}/util/words/dict/nouns.txt"
+fi
+noun_lines=`wc -l $noun_file | awk '{ print $1 }'`
+
+random_phrase () {
+  adjective_index=$((RANDOM*RANDOM%$adjective_lines+1));
+  noun_index=$((RANDOM*RANDOM%$noun_lines+1));
+  echo `sed -n "$adjective_index p" "${adjective_file}"`'-'`sed -n "$noun_index p" "${noun_file}"`
+}
