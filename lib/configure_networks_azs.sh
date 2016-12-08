8463cf4ca11d9452c3079f6d2a09fb1e6389ccc6
Chuck D'Antonio <c_dantonio@acm.org>
2016-12-08 09:50:05 -0500

    Removed trace instruction

--- lib/configure_networks_azs.sh
+++ lib/configure_networks_azs.sh
@@ -3,9 +3,7 @@
 configure_networks_azs () {
   product=$1
   settings=$2
-
-  set -o xtrace
-
+  
   GUID=`product_guid $1`
   login_ops_manager > /dev/null
   curl -qsf --insecure -X PUT "${OPS_MANAGER_API_ENDPOINT}/staged/products/${GUID}/networks_and_azs" -H "Authorization: Bearer ${UAA_ACCESS_TOKEN}" -H "Accept: application/json" \
