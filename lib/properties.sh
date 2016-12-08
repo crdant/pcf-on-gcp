59c4b1f3efb3a72b9ff8f38caec52c068b99564e
Chuck D'Antonio <c_dantonio@acm.org>
2016-12-08 10:34:33 -0500

    Combined get_propeties/set_properties source files into one so both functions are available together

---
+++ lib/properties.sh
@@ -0,0 +1,20 @@
+# get/set product properties on operations manager
+
+get_properties () {
+  product=$1
+
+  GUID=`product_guid $1`
+  login_ops_manager > /dev/null
+  curl -qsf --insecure "${OPS_MANAGER_API_ENDPOINT}/staged/products/${GUID}/properties" -H "Authorization: Bearer ${UAA_ACCESS_TOKEN}" -H "Accept: application/json" \
+    -H "Content-Type: application/json"
+}
+
+set_properties () {
+  product=$1
+  properties=$2
+
+  GUID=`product_guid $1`
+  login_ops_manager > /dev/null
+  curl -q --insecure -X PUT "${OPS_MANAGER_API_ENDPOINT}/staged/products/${GUID}/properties" -H "Authorization: Bearer ${UAA_ACCESS_TOKEN}" -H "Accept: application/json" \
+    -H "Content-Type: application/json" -d "${properties}"
+}
