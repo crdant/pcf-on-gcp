57babc5b2f4fb2ad9762a76a5d8bf3cf06d9f207
Chuck D'Antonio <c_dantonio@acm.org>
2016-12-08 09:26:21 -0500

    Added script to get properties and debug script that sets properties after seeing defects in the get_properties function.

--- lib/set_properties.sh
+++ lib/set_properties.sh
@@ -6,6 +6,6 @@ set_properties () {
 
   GUID=`product_guid $1`
   login_ops_manager > /dev/null
-  curl -qs --insecure -X PUT "${OPS_MANAGER_API_ENDPOINT}/staged/products/${PCF_GUID}/properties" -H "Authorization: Bearer ${UAA_ACCESS_TOKEN}" -H "Accept: application/json" \
+  curl -qsf --insecure -X PUT "${OPS_MANAGER_API_ENDPOINT}/staged/products/${GUID}/properties" -H "Authorization: Bearer ${UAA_ACCESS_TOKEN}" -H "Accept: application/json" \
     -H "Content-Type: application/json" -d "${properties}"
 }
