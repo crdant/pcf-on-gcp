b0371141568e3009d923252b5b18886df3aee9d9
Chuck D'Antonio <c_dantonio@acm.org>
2016-12-08 09:27:11 -0500

    Debugged function to configure networks and availability zones which was only affecting PCF

--- lib/configure_networks_azs.sh
+++ lib/configure_networks_azs.sh
@@ -4,8 +4,10 @@ configure_networks_azs () {
   product=$1
   settings=$2
 
+  set -o xtrace
+
   GUID=`product_guid $1`
   login_ops_manager > /dev/null
-  curl -qsf --insecure -X PUT "${OPS_MANAGER_API_ENDPOINT}/staged/products/${PCF_GUID}/networks_and_azs" -H "Authorization: Bearer ${UAA_ACCESS_TOKEN}" -H "Accept: application/json" \
+  curl -qsf --insecure -X PUT "${OPS_MANAGER_API_ENDPOINT}/staged/products/${GUID}/networks_and_azs" -H "Authorization: Bearer ${UAA_ACCESS_TOKEN}" -H "Accept: application/json" \
     -H "Content-Type: application/json" -d "${settings}"
 }
