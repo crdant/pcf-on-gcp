8a1bc1960847f413b089b3d30ac32779d37236f8
Chuck D'Antonio <c_dantonio@acm.org>
2016-12-20 12:27:24 -0500

    Generatlized to log into any ops manger when given a hostname, stays with manager.gcp.crdant.io as a default.

--- lib/login_ops_manager.sh
+++ lib/login_ops_manager.sh
@@ -1,9 +1,14 @@
 # log into the operations manager API using UAA
 #    reference $UAA_ACCESS_TOKEN as the bearer token for API calls
 
-invalid_login () {
-  status_code=`curl -qsLf -I --insecure "${OPS_MANAGER_FQDN}/uaa/tokens_expiration"  -H "Authorization: Bearer ${UAA_ACCESS_TOKEN}" -H "Accept: application/json"  -w "%{http_code}\n" -o /dev/null`
-  if [ "$status_code" != "200" ] ; then
+valid_login () {
+  ops_manager_fqdn=$1
+  if [ -z "${ops_manager_fqdn}" ] ; then
+    ops_manager_fqdn=${OPS_MANAGER_FQDN}
+  fi
+
+  status_code=`curl -qsLf -I --insecure "${OPS_MANAGER_FQDN}/api/v0/uaa/tokens_expiration"  -H "Authorization: Bearer ${UAA_ACCESS_TOKEN}" -H "Accept: application/json"  -w "%{http_code}\n" -o /dev/null`
+  if [ "$status_code" = "200" ] ; then
     return 0
   else
     return 1
@@ -11,18 +16,28 @@ invalid_login () {
 }
 
 do_login () {
-  uaac target "${OPS_MANAGER_FQDN}/uaa" --skip-ssl-validation
+  ops_manager_fqdn=$1
+  if [ -z "${ops_manager_fqdn}" ] ; then
+    ops_manager_fqdn=${OPS_MANAGER_FQDN}
+  fi
+
+  uaac target "${ops_manager_fqdn}/uaa" --skip-ssl-validation
   uaac token owner get opsman admin --secret='' --password="${ADMIN_PASSWORD}"
   UAA_ACCESS_TOKEN=`uaac context | grep "access_token" | sed '1s/^[ \t]*access_token: //'`
 }
 
 login_ops_manager () {
-  UAA_ACCESS_TOKEN=`uaac context | grep "access_token" | sed '1s/^[ \t]*access_token: //'`
-  if [ -z "$UAA_ACCESS_TOKEN" ] ; then
-    do_login
+  ops_manager_fqdn=$1
+  if [ -z "$ops_manager_fqdn" ] ; then
+    ops_manager_fqdn=${OPS_MANAGER_FQDN}
   fi
 
-  if invalid_login ; then
-    do_login
+  UAA_ACCESS_TOKEN=`uaac context | grep "access_token" | sed '1s/^[ \t]*access_token: //'`
+  if [ -z "$UAA_ACCESS_TOKEN" ] ; then
+    do_login $ops_manager_fqdn
+  fi
+
+  if ! valid_login $ops_manager_fqdn ; then
+    do_login $ops_manager_fqdn
   fi
 }
