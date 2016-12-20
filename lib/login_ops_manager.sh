a5e99afdeccc7cc2e4ce3d49586f3ee7f35a8955
Chuck D'Antonio <c_dantonio@acm.org>
2016-12-20 10:58:05 -0500

    Reworked valid_login to follow unix convention (return 0 is good).

--- lib/login_ops_manager.sh
+++ lib/login_ops_manager.sh
@@ -1,9 +1,9 @@
 # log into the operations manager API using UAA
 #    reference $UAA_ACCESS_TOKEN as the bearer token for API calls
 
-validate_login () {
-  status_code=`curl -qs -I --insecure "${OPS_MANAGER_FQDN}/uaa/tokens_expiration"  -H "Authorization: Bearer ${UAA_ACCESS_TOKEN}" -H "Accept: application/json"  -w "%{http_code}\n"`
-  if [ "$status_code" = "401" ] ; then
+invalid_login () {
+  status_code=`curl -qsLf -I --insecure "${OPS_MANAGER_FQDN}/uaa/tokens_expiration"  -H "Authorization: Bearer ${UAA_ACCESS_TOKEN}" -H "Accept: application/json"  -w "%{http_code}\n" -o /dev/null`
+  if [ "$status_code" != "200" ] ; then
     return 0
   else
     return 1
@@ -22,8 +22,7 @@ login_ops_manager () {
     do_login
   fi
 
-  validate_login
-  if [ $? -eq 0 ] ; then
+  if invalid_login ; then
     do_login
   fi
 }
