a95492d7039f332b3df824ef949bede7be301455
Chuck D'Antonio <c_dantonio@acm.org>
2016-12-20 10:31:48 -0500

    Removed login from unlock_ops_manager call which should not need to be authenticated per the docs (in fact it can't be because UAA isn't available until it is unlocked).

--- lib/unlock_ops_manager.sh
+++ lib/unlock_ops_manager.sh
@@ -1,6 +1,5 @@
 # log unlock the operations manager after restart
 
 unlock_ops_manager () {
-    login_ops_manager > /dev/null
-    curl -q --insecure "${OPS_MANAGER_API_ENDPOINT}/unlock" -X PUT -H "Content-Type: application/json" -d '{"passphrase": "$DECRYPTION_PASSPHRASE"}'
+  curl -qsLf --insecure "${OPS_MANAGER_API_ENDPOINT}/unlock" -X PUT -H "Content-Type: application/json" -d '{"passphrase": "$DECRYPTION_PASSPHRASE"}'
 }
