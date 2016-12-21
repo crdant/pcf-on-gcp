e6ae6c7956180eea4bc95c941dbca3db7dc1185f
Chuck D'Antonio <c_dantonio@acm.org>
2016-12-21 11:28:28 -0500

    Moved setup into the function random_phrase.

--- lib/random_phrase.sh
+++ lib/random_phrase.sh
@@ -1,22 +1,22 @@
 # get a random adjective-noun phrase using the same source files as random routes in CF
 #    outputs the phrase
 
-CF_CLI_GITHUB_RAW_ROOT="https://raw.githubusercontent.com/cloudfoundry/cli/master"
-adjective_file="$WORKDIR/adjectives.txt"
-noun_file="$WORKDIR/nouns.txt"
-
-if [ ! -f "${adjective_file}" ] ; then
-  curl -qsLf -o "${adjective_file}" "${CF_CLI_GITHUB_RAW_ROOT}/util/words/dict/adjectives.txt"
-fi
-adjective_lines=`wc -l $adjective_file | awk '{ print $1 }'`
-
-
-if [ ! -f "${noun_file}" ] ; then
-  curl -qsLf -o "${noun_file}" "${CF_CLI_GITHUB_RAW_ROOT}/util/words/dict/nouns.txt"
-fi
-noun_lines=`wc -l $noun_file | awk '{ print $1 }'`
-
 random_phrase () {
+  CF_CLI_GITHUB_RAW_ROOT="https://raw.githubusercontent.com/cloudfoundry/cli/master"
+  adjective_file="$WORKDIR/adjectives.txt"
+  noun_file="$WORKDIR/nouns.txt"
+
+  if [ ! -f "${adjective_file}" ] ; then
+    curl -qsLf -o "${adjective_file}" "${CF_CLI_GITHUB_RAW_ROOT}/util/words/dict/adjectives.txt"
+  fi
+  adjective_lines=`wc -l $adjective_file | awk '{ print $1 }'`
+
+
+  if [ ! -f "${noun_file}" ] ; then
+    curl -qsLf -o "${noun_file}" "${CF_CLI_GITHUB_RAW_ROOT}/util/words/dict/nouns.txt"
+  fi
+  
+  noun_lines=`wc -l $noun_file | awk '{ print $1 }'`
   adjective_index=$((RANDOM*RANDOM%$adjective_lines+1));
   noun_index=$((RANDOM*RANDOM%$noun_lines+1));
   echo `sed -n "$adjective_index p" "${adjective_file}"`'-'`sed -n "$noun_index p" "${noun_file}"`
