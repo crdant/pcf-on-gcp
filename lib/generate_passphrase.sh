74b3075f5ef47bf35ca9f6d5a1ecc9cd19d5e472
Chuck D'Antonio <c_dantonio@acm.org>
2016-12-21 11:30:22 -0500

    Moved setup into the generate_passphrase function.

--- lib/generate_passphrase.sh
+++ lib/generate_passphrase.sh
@@ -1,24 +1,25 @@
 # get a random adjective-noun phrase using the same source files as random routes in CF
 #    outputs the phrase
 
-CF_CLI_GITHUB_RAW_ROOT="https://raw.githubusercontent.com/cloudfoundry/cli/master"
-adjective_file="$WORKDIR/adjectives.txt"
-noun_file="$WORKDIR/nouns.txt"
-delimiter="-"
-
-if [ ! -f "${adjective_file}" ] ; then
-  curl -qsLf -o "${adjective_file}" "${CF_CLI_GITHUB_RAW_ROOT}/util/words/dict/adjectives.txt"
-fi
-adjective_lines=`wc -l ${adjective_file} | awk '{ print $1 }'`
-
-
-if [ ! -f "${noun_file}" ] ; then
-  curl -qsLf -o "${noun_file}" "${CF_CLI_GITHUB_RAW_ROOT}/util/words/dict/nouns.txt"
-fi
-noun_lines=`wc -l ${noun_file} | awk '{ print $1 }'`
-
 generate_passphrase () {
   words=$1
+
+
+  CF_CLI_GITHUB_RAW_ROOT="https://raw.githubusercontent.com/cloudfoundry/cli/master"
+  adjective_file="$WORKDIR/adjectives.txt"
+  noun_file="$WORKDIR/nouns.txt"
+  delimiter="-"
+
+  if [ ! -f "${adjective_file}" ] ; then
+    curl -qsLf -o "${adjective_file}" "${CF_CLI_GITHUB_RAW_ROOT}/util/words/dict/adjectives.txt"
+  fi
+  adjective_lines=`wc -l ${adjective_file} | awk '{ print $1 }'`
+
+
+  if [ ! -f "${noun_file}" ] ; then
+    curl -qsLf -o "${noun_file}" "${CF_CLI_GITHUB_RAW_ROOT}/util/words/dict/nouns.txt"
+  fi
+  noun_lines=`wc -l ${noun_file} | awk '{ print $1 }'`
 
   counter=0
   passphrase=""
