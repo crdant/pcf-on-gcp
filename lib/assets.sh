7291a0fc0b19208c04e4f0cdb6ff175cfc6bd2ae
Chuck D'Antonio <c_dantonio@acm.org>
2016-12-22 15:51:53 -0500

    Fixed uploading assets so it would get the passphrase correctly

--- lib/assets.sh
+++ lib/assets.sh
@@ -5,12 +5,14 @@ download_installation_assets () {
   ops_manager_fqdn=$1
   archive_file=$2
   login_ops_manager ${ops_manager_fqdn} > /dev/null
-  curl -qLf --insecure "${ops_manager_fqdn}/api/v0/installation_asset_collection" -H "Authorization: Bearer ${UAA_ACCESS_TOKEN}" -o "${archive_file}"
+  curl -qLf --insecure "https://${ops_manager_fqdn}/api/v0/installation_asset_collection" -H "Authorization: Bearer ${UAA_ACCESS_TOKEN}" -o "${archive_file}"
 }
 
 upload_installation_assets () {
   ops_manager_fqdn=$1
   archive_file=$2
+
   login_ops_manager ${ops_manager_fqdn} > /dev/null
-  curl -qfL --insecure -X PUT "${ops_manager_fqdn}/api/v0/installation_asset_collection" -H "Authorization: Bearer ${UAA_ACCESS_TOKEN}" -H "Accept: application/json" -d "@{archive_file}"
+  curl -qvf --insecure "https://${ops_manager_fqdn}/api/v0/installation_asset_collection" -H "Authorization: Bearer ${UAA_ACCESS_TOKEN}" \
+    -F "installation[file]=@${archive_file}" -F "passphrase=$DECRYPTION_PASSPHRASE"
 }
