25dbba0103d3a461b21e0463b0c7b750d2acb37a
Chuck D'Antonio <c_dantonio@acm.org>
2016-12-22 17:20:59 -0500

    Added support for downloading and installing a stemcell

--- lib/eula.sh
+++ lib/eula.sh
@@ -7,7 +7,7 @@ accept_eula () {
   releases_url="https://network.pivotal.io/api/v2/products/${product}/releases"
 
   if [ "${accepted}" = "yes" ]; then
-    eula_url=`curl -qsLf -H "Authorization: Token $PIVNET_TOKEN" $releases_url | jq --raw-output ".releases[] | select( .version == \"$version\" ) ._links .eula_acceptance .href"`
+    eula_url=`curl -qsLf -H "Authorization: Token $PIVNET_TOKEN" $releases_url | jq --raw-output "[.releases[] | select( .version | startswith(\"$version\") ) ] | max_by(.release_date) ._links .eula_acceptance .href"`
     accepted_at=`curl -qsLf -X POST -d "" -H "Authorization: Token $PIVNET_TOKEN" $eula_url | jq --raw-output '.accepted_at'`
     echo "Accepted EULA for $product at $accepted_at"
   else
--- lib/products.sh
+++ lib/products.sh
@@ -60,13 +60,40 @@ download_addon () {
   addon_file="$WORKDIR/${product}-${version_token}.tgz"
 
   if [ ! -f $addon_file ] ; then
-    files_url=`curl -qsLf -H "Authorization: Token $PIVNET_TOKEN" "$releases_url" | jq --raw-output ".releases[] | select( .version == \"$version\" ) ._links .product_files .href"`
-    download_post_url=`curl -qsLf -H "Authorization: Token $PIVNET_TOKEN" $files_url | jq --raw-output ".product_files[] | select( .aws_object_key | endswith(\"tgz\") ) ._links .download .href"`
+    files_url=`curl -qsLf -H "Authorization: Token $PIVNET_TOKEN" "$releases_url" | jq --raw-output ".releases[] |  ._links .product_files .href"`
+    download_post_url=`curl -qsLf -H "Authorization: Token $PIVNET_TOKEN" $files_url | jq --raw-output ".product_files[] | select( .version == \"$version\" ) ._links .download .href"`
     download_url=`curl -qsLf -X POST -d "" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Token $PIVNET_TOKEN" $download_post_url -w "%{url_effective}\n"`
     curl -qsLf -o $tile_file $download_url
   fi
 
   echo $addon_file
+}
+
+download_stemcell () {
+  version=$1
+  product="stemcells"
+  version_token=`echo ${version} | tr . - | tr ' ' - | tr -d ')' | tr -d '('`
+  numeric_version=`echo ${version} | sed 's/[^0-9.]*//g'`
+  releases_url="https://network.pivotal.io/api/v2/products/${product}/releases"
+
+  files_url=`curl -qsLf -H "Authorization: Token $PIVNET_TOKEN" "$releases_url" | jq --raw-output "[.releases[] | select( .version | startswith(\"$version\") ) ] | max_by(.release_date) ._links .product_files .href"`
+  download_post_url=`curl -qsLf -H "Authorization: Token $PIVNET_TOKEN" $files_url | jq --raw-output ".product_files[] | select( .aws_object_key | contains(\"google\") ) ._links .download .href"`
+  download_url=`curl -qsLf -X POST -d "" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Token $PIVNET_TOKEN" $download_post_url -w "%{url_effective}\n"`
+  stemcell_file=`echo ${download_url##*/} | cut -d \? -f 1`
+  if [ ! -f $stemcell_file ] ; then
+    curl -qsLf -o "${WORKDIR}/$stemcell_file" $download_url
+  fi
+
+  echo $stemcell_file
+}
+
+upload_stemcell () {
+  stemcell_file=$1
+
+  login_ops_manager > /dev/null
+  curl -q --insecure -X POST "${OPS_MANAGER_API_ENDPOINT}/stemcells" -H "Authorization: Bearer ${UAA_ACCESS_TOKEN}" \
+    -H "Accept: application/json" -F "stemcell[file]=@${stemcell_file}"
+
 }
 
 upload_tile () {
