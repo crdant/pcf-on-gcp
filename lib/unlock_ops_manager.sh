3d98dc3e65783a8a4f5718845ba5191554f951f9
Chuck D'Antonio <c_dantonio@acm.org>
2016-12-22 10:48:47 -0500

    Fixed quoting issue that was preventing the unlock function from unlocking operations manager

--- lib/unlock_ops_manager.sh
+++ lib/unlock_ops_manager.sh
@@ -1,5 +1,5 @@
 # log unlock the operations manager after restart
 
 unlock_ops_manager () {
-  curl -qsLf --insecure "${OPS_MANAGER_API_ENDPOINT}/unlock" -X PUT -H "Content-Type: application/json" -d '{"passphrase": "$DECRYPTION_PASSPHRASE"}'
+  curl -qsLf --insecure "${OPS_MANAGER_API_ENDPOINT}/unlock" -X PUT -H "Content-Type: application/json" -d "{\"passphrase\": \"$DECRYPTION_PASSPHRASE\"}"
 }
