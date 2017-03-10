074095455e3f0668b67be8faadefe9beab0242c2
Chuck D'Antonio <c_dantonio@acm.org>
2017-03-10 11:17:05 -0500

    Continued refining based on attempts to upgrade 1.8 to 1.9. More learnings coming from 1.9 to 1.10 upgrade.

--- upgrade.sh
+++ upgrade.sh
@@ -9,6 +9,8 @@ BASEDIR=`dirname $0`
 . "${BASEDIR}/lib/login_ops_manager.sh"
 . "${BASEDIR}/lib/assets.sh"
 . "${BASEDIR}/lib/products.sh"
+. "${BASEDIR}/lib/resources.sh"
+. "${BASEDIR}/lib/properties.sh"
 . "${BASEDIR}/lib/random_phrase.sh"
 . "${BASEDIR}/lib/generate_passphrase.sh"
 . "${BASEDIR}/lib/eula.sh"
@@ -16,6 +18,7 @@ BASEDIR=`dirname $0`
 . "${BASEDIR}/lib/networks_azs.sh"
 
 update_env () {
+  login_ops_manager
   CURRENT_PCF_VERSION=`available_products | jq --raw-output '. [] | select ( .name == "cf") .product_version'`
   CURRENT_OPS_MANAGER_VERSION=`curl -qs --insecure -H "Authorization: Bearer ${UAA_ACCESS_TOKEN}" -H "Accept: application/json" ${OPS_MANAGER_API_ENDPOINT}/diagnostic_report | jq --raw-output ".versions .release_version" | cut -d. -f1-3`
   CURRENT_OPS_MANAGER_VERSION_TOKEN=`echo ${CURRENT_OPS_MANAGER_VERSION} | tr . -`
@@ -27,6 +30,20 @@ download_assets () {
   echo "Downloading installation assets to ${INSTALLATION_ASSETS_ARCHIVE}..."
   download_installation_assets "${OPS_MANAGER_FQDN}" "${INSTALLATION_ASSETS_ARCHIVE}"
   echo "Installation assets downloaded"
+}
+
+prepare_dns () {
+  echo "Configuring DNS for Operations Manager..."
+  gcloud compute --project "${PROJECT}" addresses create "pcf-ops-manager-${DOMAIN_TOKEN}-new" --region "${REGION_1}" --no-user-output-enabled
+  CURRENT_OPS_MANAGER_ADDRESS=`gcloud compute --project "${PROJECT}" --format json addresses describe "pcf-ops-manager-${DOMAIN_TOKEN}" --region "${REGION_1}"  | jq --raw-output ".address"`
+  NEW_OPS_MANAGER_ADDRESS=`gcloud compute --project "${PROJECT}" --format json addresses describe "pcf-ops-manager-${DOMAIN_TOKEN}-new" --region "${REGION_1}"  | jq --raw-output ".address"`
+  gcloud dns record-sets transaction start -z "${DNS_ZONE}" --transaction-file="${WORKDIR}/dns-transaction-${DNS_ZONE}.xml" --no-user-output-enabled
+  gcloud dns record-sets transaction add -z "${DNS_ZONE}" --name "${OLD_OPS_MANAGER_FQDN}" --ttl "${DNS_TTL}" --type A ${CURRENT_OPS_MANAGER_ADDRESS} --transaction-file="${WORKDIR}/dns-transaction-${DNS_ZONE}.xml" --no-user-output-enabled
+  gcloud dns record-sets transaction remove -z "${DNS_ZONE}" --name "${OPS_MANAGER_FQDN}" --ttl "${DNS_TTL}" --type A ${CURRENT_OPS_MANAGER_ADDRESS} --transaction-file="${WORKDIR}/dns-transaction-${DNS_ZONE}.xml" --no-user-output-enabled
+  gcloud dns record-sets transaction add -z "${DNS_ZONE}" --name "${OPS_MANAGER_FQDN}" --ttl "${DNS_TTL}" --type A ${NEW_OPS_MANAGER_ADDRESS} --transaction-file="${WORKDIR}/dns-transaction-${DNS_ZONE}.xml" --no-user-output-enabled
+  gcloud dns record-sets transaction execute -z "${DNS_ZONE}" --transaction-file="${WORKDIR}/dns-transaction-${DNS_ZONE}.xml" --no-user-output-enabled
+  echo "Updated Operations Manager DNS for ${OPS_MANAGER_FQDN} to ${NEW_OPS_MANAGER_ADDRESS}."
+
 }
 
 new_ops_manager () {
@@ -49,17 +66,10 @@ new_ops_manager () {
   gcloud compute --project "${PROJECT}" images create "pcf-ops-manager-${OPS_MANAGER_VERSION_TOKEN}" --family "pcf-ops-manager" --description "Primary disk for Pivotal Cloud Foundry Operations Manager (v. ${OPS_MANAGER_VERSION})" --source-uri "${IMAGE_SOURCE_URI}" --no-user-output-enabled
   echo "Operations Manager image created."
 
-  # make sure we can get to it
-  echo "Configuring DNS for Operations Manager..."
-  gcloud compute --project "${PROJECT}" addresses create "pcf-ops-manager-${DOMAIN_TOKEN}-new" --region "${REGION_1}" --no-user-output-enabled
-  CURRENT_OPS_MANAGER_ADDRESS=`gcloud compute --project "${PROJECT}" --format json addresses describe "pcf-ops-manager-${DOMAIN_TOKEN}" --region "${REGION_1}"  | jq --raw-output ".address"`
-  NEW_OPS_MANAGER_ADDRESS=`gcloud compute --project "${PROJECT}" --format json addresses describe "pcf-ops-manager-${DOMAIN_TOKEN}-new" --region "${REGION_1}"  | jq --raw-output ".address"`
-  gcloud dns record-sets transaction start -z "${DNS_ZONE}" --transaction-file="${WORKDIR}/dns-transaction-${DNS_ZONE}.xml" --no-user-output-enabled
-  gcloud dns record-sets transaction add -z "${DNS_ZONE}" --name "${OLD_OPS_MANAGER_FQDN}" --ttl "${DNS_TTL}" --type A ${CURRENT_OPS_MANAGER_ADDRESS} --transaction-file="${WORKDIR}/dns-transaction-${DNS_ZONE}.xml" --no-user-output-enabled
-  gcloud dns record-sets transaction remove -z "${DNS_ZONE}" --name "${OPS_MANAGER_FQDN}" --ttl "${DNS_TTL}" --type A ${CURRENT_OPS_MANAGER_ADDRESS} --transaction-file="${WORKDIR}/dns-transaction-${DNS_ZONE}.xml" --no-user-output-enabled
-  gcloud dns record-sets transaction add -z "${DNS_ZONE}" --name "${OPS_MANAGER_FQDN}" --ttl "${DNS_TTL}" --type A ${NEW_OPS_MANAGER_ADDRESS} --transaction-file="${WORKDIR}/dns-transaction-${DNS_ZONE}.xml" --no-user-output-enabled
-  gcloud dns record-sets transaction execute -z "${DNS_ZONE}" --transaction-file="${WORKDIR}/dns-transaction-${DNS_ZONE}.xml" --no-user-output-enabled
-  echo "Updated Operations Manager DNS for ${OPS_MANAGER_FQDN} to ${OPS_MANAGER_ADDRESS}."
+  prepare_dns
+
+  echo "Shutting down existing operations manager instance..."
+  gcloud compute --project ${PROJECT} instances stop "pcf-ops-manager-${CURRENT_OPS_MANAGER_VERSION_TOKEN}-${DOMAIN_TOKEN}"  --quiet
 
   echo "Creating Operations Manager instance..."
   gcloud compute --project "${PROJECT}" instances create "pcf-ops-manager-${OPS_MANAGER_VERSION_TOKEN}-${DOMAIN_TOKEN}" --zone ${AVAILABILITY_ZONE_1} --machine-type "n1-standard-1" --subnet "pcf-${REGION_1}-${DOMAIN_TOKEN}" --private-network-ip "10.0.0.5" --address "https://www.googleapis.com/compute/v1/projects/${PROJECT}/regions/${REGION_1}/addresses/pcf-ops-manager-${DOMAIN_TOKEN}-new" --maintenance-policy "MIGRATE" --scopes ${SERVICE_ACCOUNT}="https://www.googleapis.com/auth/cloud-platform" --tags "http-server","https-server","pcf-opsmanager" --image-family "pcf-ops-manager" --boot-disk-size "200" --boot-disk-type "pd-standard" --boot-disk-device-name "pcf-operations-manager" --no-user-output-enabled
@@ -69,6 +79,7 @@ new_ops_manager () {
   # noticed I was getting 502 and 503 errors on the setup calls below, so sleeping to see if that helps
   echo "Waiting for ${DNS_TTL} seconds for Operations Manager instance to be available and DNS to be updated..."
   sleep ${DNS_TTL}
+
 }
 
 migrate_ops_manager () {
@@ -78,7 +89,6 @@ migrate_ops_manager () {
 }
 
 stemcell () {
-  login_ops_manager
   echo "Downloading latest product stemcell ${STEMCELL_VERSION}..."
   accept_eula "stemcells" "${STEMCELL_VERSION}" "yes"
   stemcell_file=`download_stemcell ${STEMCELL_VERSION}`
@@ -87,7 +97,7 @@ stemcell () {
 }
 
 cloud_foundry () {
-  if product_not_available "cf" "${PCF_VERSION}"] ; then
+  if product_not_available "cf" "${PCF_VERSION}" ; then
     accept_eula "elastic-runtime" "${PCF_VERSION}" "yes"
     echo "Downloading Cloud Foundry Elastic Runtime..."
     tile_file=`download_tile "elastic-runtime" "${PCF_VERSION}"`
@@ -97,13 +107,12 @@ cloud_foundry () {
     echo "Cloud Foundry version ${PCF_VERSION} is already available in Operations Manager at ${OPS_MANAGER_FQDN}"
   fi
   echo "Staging Cloud Foundry Elastic Runtime..."
-  stage_product "cf"
-  stemcell
+  stage_product "cf" "${PCF_VERSION}"
 
   # configure BLOB storage locations, system domain, etc. doesn't set everything yet (SSL certificate info doesn't
   # come back with a GET so it's hard to figure out how to set it)
-  PRIVATE_KEY=`cat ${WORKDIR}/pcf-router-${DOMAIN_TOKEN}.key`
-  SSL_CERT=`cat ${WORKDIR}/pcf-router-${DOMAIN_TOKEN}.crt`
+  PRIVATE_KEY=`cat ${KEYDIR}/pcf-router-${DOMAIN_TOKEN}.key`
+  SSL_CERT=`cat ${KEYDIR}/pcf-router-${DOMAIN_TOKEN}.crt`
 
   # looks funny, but it keeps us from polluting the environment
   CF_NETWORK_SETTINGS=`export DIRECTOR_NETWORK_NAME AVAILABILITY_ZONE_1 AVAILABILITY_ZONE_2 AVAILABILITY_ZONE_3; envsubst < api-calls/tile-networks-and-azs.json ; unset  DIRECTOR_NETWORK_NAME AVAILABILITY_ZONE_1 AVAILABILITY_ZONE_2 AVAILABILITY_ZONE_3`
@@ -128,14 +137,20 @@ cloud_foundry () {
   BRAIN_LBS="[ \"tcp:$SSH_LOAD_BALANCER_NAME\" ]"
   BRAIN_RESOURCES=`echo $BRAIN_RESOURCES | jq ".elb_names = $BRAIN_LBS"`
   set_resources cf diego_brain "${BRAIN_RESOURCES}"
+
+  # update the number of
+}
+
+cleanup_dns () {
+  echo "Removing DNS entries for ${OLD_OPS_MANAGER_FQDN}..."
+  gcloud dns record-sets transaction start -z "${DNS_ZONE}" --transaction-file="${WORKDIR}/dns-transaction-${DNS_ZONE}.xml" --no-user-output-enabled
+  gcloud dns record-sets transaction remove -z "${DNS_ZONE}" --name "${OLD_OPS_MANAGER_FQDN}" --ttl "${DNS_TTL}" --type A "${CURRENT_OPS_MANAGER_ADDRESS}" --transaction-file="${WORKDIR}/dns-transaction-${DNS_ZONE}.xml" --no-user-output-enabled
+  gcloud dns record-sets transaction execute -z "${DNS_ZONE}" --transaction-file="${WORKDIR}/dns-transaction-${DNS_ZONE}.xml" --no-user-output-enabled
+  echo "Removed DNS entries for ${OLD_OPS_MANAGER_FQDN}..."
 }
 
 cleanup () {
-  echo "Removing DNS entries for ${OLD_OPS_MANAGER_FQDN}..."
-  gcloud dns record-sets transaction start -z "${DNS_ZONE}" --transaction-file="${WORKDIR}/dns-transaction-${DNS_ZONE}.xml" --no-user-output-enabled
-  gcloud dns record-sets transaction remove -z "${DNS_ZONE}" --name "${OLD_OPS_MANAGER_FQDN}" --ttl "${DNS_TTL}" --type A ${CURRENT_OPS_MANAGER_ADDRESS} --transaction-file="${WORKDIR}/dns-transaction-${DNS_ZONE}.xml" --no-user-output-enabled
-  gcloud dns record-sets transaction execute -z "${DNS_ZONE}" --transaction-file="${WORKDIR}/dns-transaction-${DNS_ZONE}.xml" --no-user-output-enabled
-  echo "Removed DNS entries for ${OLD_OPS_MANAGER_FQDN}..."
+  cleanup_dns
 
   echo "Deleting old Ops Manager instance..."
   gcloud compute --project "${PROJECT}" instances delete "pcf-ops-manager-${CURRENT_OPS_MANAGER_VERSION_TOKEN}-${DOMAIN_TOKEN}" --zone ${AVAILABILITY_ZONE_1} --quiet
@@ -155,9 +170,11 @@ overrides
 setup
 echo "Started updating Cloud Foundry in ${PROJECT} from ${CURRENT_PCF_VERSION} to ${PCF_VERSION} at ${START_TIMESTAMP}..."
 # download_assets
-# new_ops_manager
-# migrate_ops_manager
-# stemcell
+new_ops_manager
+migrate_ops_manager
+login_ops_manager
+stemcell
+stage_product "cf" "${PCF_VERSION}"
 cloud_foundry
 # cleanup
 END_TIMESTAMP=`date`
