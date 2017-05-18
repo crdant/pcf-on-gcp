73fdfefa6f8ed3327f7f157b72f52ec284caf346
Chuck D'Antonio <c_dantonio@acm.org>
2017-05-18 14:29:40 -0400

    Added version checks

--- lib/products.sh
+++ lib/products.sh
@@ -41,10 +41,16 @@ download_tile () {
   releases_url="https://network.pivotal.io/api/v2/products/${product}/releases"
   tile_file="$WORKDIR/${product}-${version_token}.pivotal"
 
+  echo "Release URL: $releases_url"
+  echo "File: $tile_file"
+
   if [ ! -f $tile_file ] ; then
     files_url=`curl -qsLf -H "Authorization: Token $PIVNET_TOKEN" "$releases_url" | jq --raw-output ".releases[] | select( .version == \"$version\" ) ._links .product_files .href"`
+    echo "Files URL: $files_url"
     download_post_url=`curl -qsLf -H "Authorization: Token $PIVNET_TOKEN" $files_url | jq --raw-output ".product_files[] | select( .aws_object_key | endswith(\"pivotal\") ) ._links .download .href"`
+    echo "Download Post URL: $download_post_url"
     download_url=`curl -qsLf -X POST -d "" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Token $PIVNET_TOKEN" $download_post_url -w "%{url_effective}\n"`
+    echo "Download URL: $download_url"
     curl -qsLf -o $tile_file $download_url
   fi
 
@@ -116,11 +122,11 @@ upload_addon () {
 stage_product () {
   product=$1
   version=$2
-
+  base_version=`echo ${PCF_VERSION%-*}`
   login_ops_manager > /dev/null
 
   if [ -z "${version}" ] ; then
-    available_product=`curl -qsLf --insecure "${OPS_MANAGER_API_ENDPOINT}/available_products" -H "Authorization: Bearer ${UAA_ACCESS_TOKEN}" -H "Accept: application/json" | jq --raw-output ".[] | select ( .name == \"$product\" )"`
+    available_product=`curl -qsLf --insecure "${OPS_MANAGER_API_ENDPOINT}/available_products" -H "Authorization: Bearer ${UAA_ACCESS_TOKEN}" -H "Accept: application/json" | jq --raw-output ".[] | select ( .product_version | startswith (\"$base_version\" ) ) | select ( .name == \"$product\" )"`
     product_name=`echo $available_product | jq --raw-output ".name"`
     version=`echo $available_product | jq --raw-output ".product_version"`
   fi
