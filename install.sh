dabd9fc2fd3cc0dfe24339bf807e92f83941ecb0
Chuck D'Antonio <c_dantonio@acm.org>
2017-06-18 13:11:48 -0400

    Adapted improvements made when porting scripts to AWS

--- install.sh
+++ install.sh
@@ -2,18 +2,19 @@
 # install PCF and related products
 
 BASEDIR=`dirname $0`
+GCPDIR="${BASEDIR}/../pcf-on-gcp"
 . "${BASEDIR}/lib/env.sh"
-. "${BASEDIR}/lib/customization_hooks.sh"
 . "${BASEDIR}/personal.sh"
-. "${BASEDIR}/lib/setup.sh"
-. "${BASEDIR}/lib/login_ops_manager.sh"
-. "${BASEDIR}/lib/eula.sh"
+. "${GCPDIR}/lib/login_ops_manager.sh"
+. "${BASEDIR}/lib/ops_manager.sh"
+. "${BASEDIR}/lib/elastic_runtime.sh"
+. "${GCPDIR}/lib/eula.sh"
 . "${BASEDIR}/lib/products.sh"
-. "${BASEDIR}/lib/guid.sh"
-. "${BASEDIR}/lib/networks_azs.sh"
-. "${BASEDIR}/lib/properties.sh"
-. "${BASEDIR}/lib/resources.sh"
-. "${BASEDIR}/lib/credentials.sh"
+. "${GCPDIR}/lib/guid.sh"
+. "${GCPDIR}/lib/networks_azs.sh"
+. "${GCPDIR}/lib/properties.sh"
+. "${GCPDIR}/lib/resources.sh"
+. "${GCPDIR}/lib/credentials.sh"
 
 
 init () {
@@ -22,13 +23,13 @@ init () {
   INSTALL_RABBIT=0
   INSTALL_REDIS=0
   INSTALL_SCS=0
-  INSTALL_GCP=0
-  INSTALL_GEMFIRE=0
-  INSTALL_CONCOURSE=0
+  INSTALL_AWS=0
+  INSTALL_PCC=0
   INSTALL_IPSEC=0
-  INSTALL_STACKDRIVER=0
   INSTALL_PUSH=0
   INSTALL_ISOLATION=0
+  INSTALL_WINDOWS=0
+  INSTALL_SCHEDULER=0
 }
 
 parse_args () {
@@ -53,17 +54,14 @@ parse_args () {
           "scs")
             INSTALL_SCS=1
             ;;
-          "gcp")
-            INSTALL_GCP=1
+          "azure")
+            INSTALL_AWS=1
             ;;
-          "gemfire")
-            INSTALL_GEMFIRE=1
+          "pcc")
+            INSTALL_PCC=1
             ;;
-          "concourse")
-            INSTALL_CONCOURSE=1
-            ;;
-          "stackdriver")
-            INSTALL_STACKDRIVER=1
+          "scheduler")
+            INSTALL_SCHEDULER=1
             ;;
           "notifications")
             INSTALL_PUSH=1
@@ -74,6 +72,9 @@ parse_args () {
           "isolation")
             INSTALL_ISOLATION=1
             ;;
+          "windows")
+            INSTALL_WINDOWS=1
+            ;;
           "default")
             set_defaults
             ;;
@@ -83,13 +84,13 @@ parse_args () {
             INSTALL_RABBIT=1
             INSTALL_REDIS=1
             INSTALL_SCS=1
-            INSTALL_GCP=1
-            INSTALL_GEMFIRE=1
-            INSTALL_CONCOURSE=1
+            INSTALL_AWS=1
+            INSTALL_PCC=1
+            INSTALL_SCHEDULER=1
             INSTALL_IPSEC=1
-            INSTALL_STACKDRIVER=1
             INSTALL_PUSH=1
             INSTALL_ISOLATION=1
+            INSTALL_WINDOWS=1
             ;;
           "--help")
             usage
@@ -112,13 +113,13 @@ set_defaults () {
   INSTALL_RABBIT=1
   INSTALL_REDIS=1
   INSTALL_SCS=1
-  INSTALL_GCP=1
-  INSTALL_STACKDRIVER=1
+  INSTALL_AWS=1
+  INSTALL_PCC=1
 }
 
 usage () {
   cmd=`basename $0`
-  echo "$cmd [ pcf ] [isolation] [ mysql ] [ rabbit ] [ redis ] [ scs ] [ gcp ] [ gemfire ] [ concourse ] [ stackdriver ] [ notifications ]"
+  echo "$cmd [ pcf ] [isolation] [windows] [ mysql ] [ rabbit ] [ redis ] [ scs ] [ gcp ] [ pcc ] [ scheduler ] [ notifications ]"
 }
 
 products () {
@@ -138,24 +139,16 @@ products () {
     redis
   fi
 
+  if [ "$INSTALL_PCC" -eq 1 ] ; then
+    cloud_cache
+  fi
+
   if [ "$INSTALL_SCS" -eq 1 ] ; then
     spring_cloud_services
   fi
 
-  if [ "$INSTALL_GCP" -eq 1 ] ; then
+  if [ "$INSTALL_AWS" -eq 1 ] ; then
     service_broker
-  fi
-
-  if [ "$INSTALL_GEMFIRE" -eq 1 ] ; then
-    gemfire
-  fi
-
-  if [ "$INSTALL_CONCOURSE" -eq 1 ] ; then
-    concourse
-  fi
-
-  if [ "$INSTALL_STACKDRIVER" -eq 1 ] ; then
-    stackdriver
   fi
 
   if [ "$INSTALL_PUSH" -eq 1 ] ; then
@@ -164,6 +157,10 @@ products () {
 
   if [ "$INSTALL_ISOLATION" -eq 1 ] ; then
     isolation_segments
+  fi
+
+  if [ "$INSTALL_WINDOWS" -eq 1 ] ; then
+    windows
   fi
 
   if [ "$INSTALL_IPSEC" -eq 1 ] ; then
@@ -183,31 +180,18 @@ stemcell () {
 }
 
 cloud_foundry () {
-  if product_not_available "cf" "${PCF_VERSION}"] ; then
-    accept_eula "elastic-runtime" "${PCF_VERSION}" "yes"
-    echo "Downloading Cloud Foundry Elastic Runtime..."
-    tile_file=`download_tile "elastic-runtime" "${PCF_VERSION}"`
-    echo "Uploading Cloud Foundry Elastic Runtime..."
-    upload_tile $tile_file
-  else
-    echo "Cloud Foundry version ${PCF_VERSION} is already available in Operations Manager at ${OPS_MANAGER_FQDN}"
-  fi
-  echo "Staging Cloud Foundry Elastic Runtime..."
-  stage_product "cf"
+  add_to_install "Cloud Foundry Elastic Runtime" "${PCF_SLUG}" "${PCF_VERSION}" "${PCF_OPSMAN_SLUG}"
+  store_var PCF_GUID "${guid}"
   stemcell
 
-  # configure BLOB storage locations, system domain, etc. doesn't set everything yet (SSL certificate info doesn't
-  # come back with a GET so it's hard to figure out how to set it)
-  PRIVATE_KEY=`cat ${KEYDIR}/pcf-router-${DOMAIN_TOKEN}.key`
-  SSL_CERT=`cat ${KEYDIR}/pcf-router-${DOMAIN_TOKEN}.crt`
-
-  # looks funny, but it keeps us from polluting the environment
-  CF_NETWORK_SETTINGS=`export DIRECTOR_NETWORK_NAME AVAILABILITY_ZONE_1 AVAILABILITY_ZONE_2 AVAILABILITY_ZONE_3; envsubst < api-calls/tile-networks-and-azs.json ; unset  DIRECTOR_NETWORK_NAME AVAILABILITY_ZONE_1 AVAILABILITY_ZONE_2 AVAILABILITY_ZONE_3`
-  set_networks_azs "cf" "${CF_NETWORK_SETTINGS}"
-
-  # looks funny, but it keeps us from polluting the environment
-  PROPERTIES_JSON=`export ACCOUNT PRIVATE_KEY SSL_CERT BUILDPACKS_STORAGE_BUCKET DROPLETS_STORAGE_BUCKET RESOURCES_STORAGE_BUCKET PACKAGES_STORAGE_BUCKET GCP_ACCESS_KEY_ID GCP_SECRET_ACCESS_KEY PCF_APPS_DOMAIN PCF_SYSTEM_DOMAIN; envsubst < api-calls/elastic-runtime-properties.json ; unset ACCOUNT PRIVATE_KEY SSL_CERT BUILDPACKS_STORAGE_BUCKET DROPLETS_STORAGE_BUCKET RESOURCES_STORAGE_BUCKET PACKAGES_STORAGE_BUCKET GCP_ACCESS_KEY_ID GCP_SECRET_ACCESS_KEY PCF_APPS_DOMAIN PCF_SYSTEM_DOMAINt`
-  set_properties "cf" "${PROPERTIES_JSON}"
+  # configure the elastic runtime
+  set_networks_azs "${PCF_OPSMAN_SLUG}"
+  set_pcf_domains
+  set_pcf_networking
+  set_pcf_containers
+  set_pcf_security_acknowledgement
+  set_pcf_rds_database
+  set_pcf_advanced_features
 
   # set the load balancers resource configuration
   ROUTER_RESOURCES=`get_resources cf router`
@@ -227,117 +211,85 @@ cloud_foundry () {
 }
 
 mysql () {
-  if product_not_available "p-mysql" "${MYSQL_VERSION}" ; then
-    accept_eula "p-mysql" "${MYSQL_VERSION}" "yes"
-    echo "Downloading MySQL Service..."
-    tile_file=`download_tile "p-mysql" "${MYSQL_VERSION}"`
-    echo "Uploading MySQL Service..."
-    upload_tile $tile_file
-  fi
-  echo "Staging MySQL Service..."
-  stage_product "p-mysql"
-
-  MYSQL_NETWORK_SETTINGS=`export DIRECTOR_NETWORK_NAME AVAILABILITY_ZONE_1 AVAILABILITY_ZONE_2 AVAILABILITY_ZONE_3; envsubst < api-calls/tile-networks-and-azs.json ; unset  DIRECTOR_NETWORK_NAME AVAILABILITY_ZONE_1 AVAILABILITY_ZONE_2 AVAILABILITY_ZONE_3`
-  set_networks_azs "p-mysql" "${MYSQL_NETWORK_SETTINGS}"
+  add_to_install "Rabbit MQ Broker" "${MYSQL_SLUG}" "${MYSQL_VERSION}"
+  store_var MYSQL_GUID "${GUID}"
+  set_networks_azs "${MYSQL_SLUG}" "${TILES_NETWORK_NAME}"
 }
 
 rabbit () {
-  if product_not_available "pivotal-rabbitmq-service" "${RABBIT_VERSION}" ; then
-    accept_eula "pivotal-rabbitmq-service" "${RABBIT_VERSION}" "yes"
-    echo "Downloading Rabbit MQ Service..."
-    tile_file=`download_tile "pivotal-rabbitmq-service" "${RABBIT_VERSION}"`
-    echo "Uploading Rabbit MQ Service..."
-    upload_tile $tile_file
-  fi
-  echo "Staging Rabbit MQ Service..."
-  stage_product "p-rabbitmq"
-
-  RABBIT_NETWORK_SETTINGS=`export DIRECTOR_NETWORK_NAME AVAILABILITY_ZONE_1 AVAILABILITY_ZONE_2 AVAILABILITY_ZONE_3; envsubst < api-calls/tile-networks-and-azs.json ; unset  DIRECTOR_NETWORK_NAME AVAILABILITY_ZONE_1 AVAILABILITY_ZONE_2 AVAILABILITY_ZONE_3`
-  set_networks_azs "p-rabbitmq" "${RABBIT_NETWORK_SETTINGS}"
+  add_to_install "Rabbit MQ Broker" "${RABBIT_SLUG}" "${REDIS_VERSION}"
+  store_var REDIS_GUID "${GUID}"
+  set_networks_azs "${RABIT_SLUG}" "${SERVICES_NETWORK_NAME}"
 }
 
 redis () {
-  if product_not_available "p-redis" "${REDIS_VERSION}" ; then
-    accept_eula "p-redis" "${REDIS_VERSION}" "yes"
-    echo "Downloading REDIS Service..."
-    tile_file=`download_tile "p-redis" "${REDIS_VERSION}"`
-    echo "Uploading REDIS Service..."
-    upload_tile $tile_file
-  fi
-  echo "Staging REDIS Service..."
-  stage_product "p-redis"
+  add_to_install "Redis Service Broker" "${REDIS_SLUG}" "${REDIS_VERSION}"
+  store_var REDIS_GUID "${GUID}"
+  set_networks_azs "${REDIS_SLUG}" "${SERVICES_NETWORK_NAME}"
+}
 
-  REDIS_NETWORK_SETTINGS=`export DIRECTOR_NETWORK_NAME AVAILABILITY_ZONE_1 AVAILABILITY_ZONE_2 AVAILABILITY_ZONE_3; envsubst < api-calls/tile-networks-and-azs.json ; unset  DIRECTOR_NETWORK_NAME AVAILABILITY_ZONE_1 AVAILABILITY_ZONE_2 AVAILABILITY_ZONE_3`
-  set_networks_azs "p-redis" "${REDIS_NETWORK_SETTINGS}"
+cloud_cache () {
+  add_to_install "Spring Cloud Services" "${PCC_SLUG}" "${PCC_VERSION}"
+  store_var PCC_GUID "${GUID}"
+  set_networks_azs "${PCC_SLUG}" "${TILES_NETWORK_NAME}"
 }
 
 spring_cloud_services () {
-  if product_not_available "p-spring-cloud-services" "${SCS_VERSION}" ; then
-    accept_eula "p-spring-cloud-services" "${SCS_VERSION}" "yes"
-    echo "Downloading Spring Cloud Services..."
-    tile_file=`download_tile "p-spring-cloud-services" "${SCS_VERSION}"`
-    echo "Uploading Spring Cloud Services..."
-    upload_tile $tile_file
-  fi
-  echo "Staging Spring Cloud Services..."
-  stage_product "p-spring-cloud-services"
-
-  SCS_NETWORK_SETTINGS=`export DIRECTOR_NETWORK_NAME AVAILABILITY_ZONE_1 AVAILABILITY_ZONE_2 AVAILABILITY_ZONE_3; envsubst < api-calls/tile-networks-and-azs.json ; unset  DIRECTOR_NETWORK_NAME AVAILABILITY_ZONE_1 AVAILABILITY_ZONE_2 AVAILABILITY_ZONE_3`
-  set_networks_azs "p-spring-cloud-services" "${SCS_NETWORK_SETTINGS}"
+  add_to_install "Spring Cloud Services" "${SCS_SLUG}" "${SCS_VERSION}"
+  store_var SCS_GUID "${GUID}"
+  set_networks_azs "${SCS_SLUG}" "${TILES_NETWORK_NAME}"
 }
 
 service_broker () {
-  if product_not_available "gcp-service-broker" "${GCP_VERSION_NUM}" ; then
-    # download the broker and make it available
-    accept_eula "gcp-service-broker" "${GCP_VERSION}" "yes"
-    echo "Downloading GCP Service Broker..."
-    tile_file=`download_tile "gcp-service-broker" "${GCP_VERSION}"`
-    echo "Uploading GCP Service Broker..."
-    upload_tile $tile_file
-  fi
-  echo "Staging GCP Service Broker..."
-  stage_product "gcp-service-broker"
+  add_to_install "AWS Service Broker" "${SERVICE_BROKER_SLUG}" "${SERVICE_BROKER_VERSION}"
+  store_var SERVICE_BROKER_GUID "${GUID}"
 
   # since sed works a line at a time, translate newlines to a character that isn't Base64, then
   # replace that character with an escaped newline
   CLIENT_KEY=`cat "${KEYDIR}/gcp-service-broker-db-client.key" | tr '\n' '%' | sed 's/%/\\\n/g'`
   CLIENT_CERT=`cat "${KEYDIR}/gcp-service-broker-db-client.crt" | tr '\n' '%' | sed 's/%/\\\n/g'`
   SERVER_CERT=`cat "${KEYDIR}/gcp-service-broker-db-server.crt" | tr '\n' '%' | sed 's/%/\\\n/g'`
-  SERVICE_ACCOUNT_CREDENTIALS=`cat "${KEYDIR}/${PROJECT}-service-broker-${DOMAIN_TOKEN}.json" | jq -c '.' | sed 's/\"/\\\"/g'`
+  SERVICE_ACCOUNT_CREDENTIALS=`cat "${KEYDIR}/${PROJECT}-service-broker-${SUBDOMAIN_TOKEN}.json" | jq -c '.' | sed 's/\"/\\\"/g'`
   BROKER_DB_HOST=`cat "${WORKDIR}/gcp-service-broker-db.ip"`
 
-  GCP_NETWORK_SETTINGS=`export DIRECTOR_NETWORK_NAME AVAILABILITY_ZONE_1 AVAILABILITY_ZONE_2 AVAILABILITY_ZONE_3; envsubst < api-calls/tile-networks-and-azs.json ; unset  DIRECTOR_NETWORK_NAME AVAILABILITY_ZONE_1 AVAILABILITY_ZONE_2 AVAILABILITY_ZONE_3`
-  set_networks_azs "gcp-service-broker" "${GCP_NETWORK_SETTINGS}"
+  set_networks_azs "gcp-service-broker" "${TILES_NETWORK_NAME}"
 
   PROPERTIES_JSON=`export SERVICE_ACCOUNT_CREDENTIALS BROKER_DB_HOST BROKER_DB_USER BROKER_DB_USER_PASSWORD CLIENT_KEY CLIENT_CERT SERVER_CERT ; envsubst < api-calls/gcp-service-broker-properties.json ; unset SERVICE_ACCOUNT_CREDENTIALS BROKER_DB_HOST BROKER_DB_USER BROKER_DB_USER_PASSWORD CLIENT_KEY CLIENT_CERT SERVER_CERT`
   set_properties "gcp-service-broker" "${PROPERTIES_JSON}"
 
 }
 
-gemfire () {
-  if product_not_available "p-gemfire" "${GEM_VERSION}" ; then
-    accept_eula "p-gemfire" "${GEM_VERSION}" "yes"
-    echo "Downloading Gemfire..."
-    tile_file=`download_tile "p-gemfire" "${GEM_VERSION}"`
-    echo "Uploading Gemfire..."
-    upload_tile $tile_file
-  fi
-  echo "Staging Gemfire..."
-  stage_product "p-gemfire"
-  GEM_GUID=`product_guid "p-gemfire"`
+windows () {
+  add_to_install "Runtime for Windows" "${WINDOWS_SLUG}" "${WINDOWS_VERSION}"
+  store_var WINDOWS_GUID "${GUID}"
+  windows_stemcell
 }
 
-concourse () {
-  if product_not_available "p-concourse" "${CONCOURSE_VERSION}" ; then
-    accept_eula "p-concourse" "${CONCOURSE_VERSION}" "yes"
-    echo "Downloading Concourse..."
-    tile_file=`download_tile "p-concourse" "${CONCOURSE_VERSION}"`
-    echo "Uploading Concourse..."
-    upload_tile $tile_file
-  fi
-  echo "Staging Concourse..."
-  stage_product "p-concourse"
-  CONCOURSE_GUID=`product_guid "p-concourse"`
+windows_stemcell () {
+  login_ops_manager
+  echo "Downloading latest Windows stemcell ${WINDOWS_STEMCELL_VERSION}..."
+  accept_eula "stemcells-windows-server" ${WINDOWS_STEMCELL_VERSION} "yes"
+  stemcell_file=`download_stemcell ${WINDOWS_STEMCELL_VERSION}`
+  echo "Uploading Windows stemcell to Operations Manager..."
+  upload_stemcell $stemcell_file
+}
+
+push_notifications () {
+  add_to_install "Push Notifications" "${PUSH_SLUG}" "${PUSH_VERSION}"
+  store_var PUSH_GUID "${GUID}"
+  set_networks_azs "${PUSH_SLUG}" "${TILES_NETWORK_NAME}"
+}
+
+isolation_segments () {
+  add_to_install "Isolation Segments" "${ISOLATION_SLUG}" "${ISOLATION_VERSION}"
+  store_var ISOLATION_GUID "${GUID}"
+  set_networks_azs "${ISOLATION_SLUG}"
+}
+
+scheduler () {
+  add_to_install "Isolation Segments" "${SCHEDULER_SLUG}" "${SCHEDULER_VERSION}"
+  store_var ISOLATION_GUID "${GUID}"
+  set_networks_azs "${SCHEDULER_SLUG}" "${TILES_NETWORK_NAME}"
 }
 
 ipsec () {
@@ -348,54 +300,27 @@ ipsec () {
   upload_addon $addon_file
 }
 
-stackdriver () {
-  if product_not_available "gcp-stackdriver-nozzle" "${STACKDRIVER_VERSION_NUM}" ; then
+add_to_install() {
+  product_name=${1}
+  pivnet_slug="${2}"
+  version="${3}"
+  opsman_slug="${4}"
+
+  if [ -z "${opsman_slug}" ] ; then
+    opsman_slug="${2}"
+  fi
+
+  if product_not_available "${pivnet_slug}" "${version}" ; then
     # download the broker and make it available
-    accept_eula "gcp-stackdriver-nozzle" "${STACKDRIVER_VERSION}" "yes"
-    echo "Downloading GCP Stackdriver Nozzle..."
-    tile_file=`download_tile "gcp-stackdriver-nozzle" "${STACKDRIVER_VERSION}"`
-    echo "Uploading GCP Stackdriver Nozzle..."
+    accept_eula "${pivnet_slug}" "${version}" "yes"
+    echo "Downloading ${product_name}..."
+    tile_file=`download_tile "${pivnet_slug}" "${version}"`
+    echo "Uploading ${product_name}..."
     upload_tile $tile_file
   fi
-  echo "Staging GCP Stackdriver Nozzle..."
-  stage_product "gcp-stackdriver-nozzle"
-
-  # create UAA user
-  uaac target "https://uaa.${PCF_SYSTEM_DOMAIN}" --skip-ssl-validation
-  # NOTE: the secret being set here will not work, it is not correct and the correct one does not appear
-  #       to be available without decoding installation.yml...stay tuned
-  local uaa_admin_secret=`get_credential cf .uaa.admin_client_credentials`
-  uaac token client get admin -s "${uaa_admin_secret}"
-  uaac -t user add stackdriver-nozzle --password ${STACKDRIVER_NOZZLE_PASSWORD} --emails na
-  # these probably need Cloud Foundry installed before you can do anything with them
-  uaac -t member add cloud_controller.admin_read_only stackdriver-nozzle
-  uaac -t member add doppler.firehose stackdriver-nozzle
-}
-
-push_notifications () {
-  if product_not_available "push-notification-service" "${PUSH_VERSION}" ; then
-    # download the broker and make it available
-    accept_eula "push-notification-service" "${PUSH_VERSION}" "yes"
-    echo "Downloading Push Notifications Service..."
-    tile_file=`download_tile "push-notification-service" "${PUSH_VERSION}"`
-    echo "Uploading Push Notifications Service..."
-    upload_tile $tile_file
-  fi
-  echo "Staging Push Notifications Service..."
-  stage_product "push-notification-service"
-}
-
-isolation_segments () {
-  if product_not_available "isolation-segment" "${ISOLATION_VERSION}" ; then
-    # download the broker and make it available
-    accept_eula "isolation-segment" "${ISOLATION_VERSION}" "yes"
-    echo "Downloading Isolation Segments..."
-    tile_file=`download_tile "isolation-segment" "${ISOLATION_VERSION}"`
-    echo "Uploading Isolation Segments..."
-    upload_tile $tile_file
-  fi
-  echo "Staging Isolation Segments..."
-  stage_product "isolation-segment"
+  echo "Staging ${product_name}..."
+  stage_product "${opsman_slug}"
+  GUID=`product_guid "${opsman_slug}"`
 }
 
 START_TIMESTAMP=`date`
@@ -403,12 +328,11 @@ START_SECONDS=`date +%s`
 init
 parse_args $@
 prepare_env
-overrides
-echo "Started installing Cloud Foundry components in Google Cloud Platform project ${PROJECT} at ${START_TIMESTAMP}..."
-setup
+set_versions
+echo "Started installing Cloud Foundry components on Amazon Web Services for ${SUBDOMAIN} at ${START_TIMESTAMP}..."
 login_ops_manager
 products
 END_TIMESTAMP=`date`
 END_SECONDS=`date +%s`
 ELAPSED_TIME=`echo $((END_SECONDS-START_SECONDS)) | awk '{print int($1/60)":"int($1%60)}'`
-echo "Completed installing Cloud Foundry components in Google Cloud Platform project ${PROJECT} at ${END_TIMESTAMP} (elapsed time ${ELAPSED_TIME})."
+echo "Completed installing Cloud Foundry components in Amazon Web Services for ${SUBDOMAIN} at ${END_TIMESTAMP} (elapsed time ${ELAPSED_TIME})."
