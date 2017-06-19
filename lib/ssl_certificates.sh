150c4db607ad465465c18e57cdb370aa8a9074a4
Chuck D'Antonio <c_dantonio@acm.org>
2017-06-19 16:58:27 -0400

    Added certificate for Rabbit

--- lib/ssl_certificates.sh
+++ lib/ssl_certificates.sh
@@ -35,6 +35,23 @@ ssl_certs () {
   echo "SSL certificate for CF router created and stored at ${KEYDIR}/pcf-router-${SUBDOMAIN_TOKEN}.crt, private key stored at ${KEYDIR}/pcf-router-${SUBDOMAIN_TOKEN}.key."
   echo "The certificate is a wildcard for the following domains: ${ALT_NAMES}"
 
+  echo "Creating SSL certificate for Rabbit MQ..."
+
+  COMMON_NAME="*.${SUBDOMAIN}"
+  COUNTRY="US"
+  STATE="MA"
+  CITY="Cambridge"
+  ORGANIZATION="${DOMAIN}"
+  ORG_UNIT="Cloud Foundry Router"
+  EMAIL="${ACCOUNT}"
+  ALT_NAMES="DNS:*.${SUBDOMAIN},DNS:*.${PCF_SYSTEM_DOMAIN},DNS:*.${PCF_APPS_DOMAIN},DNS:*.login.${PCF_SYSTEM_DOMAIN},DNS:*.uaa.${PCF_SYSTEM_DOMAIN}"
+  SUBJECT="/C=${COUNTRY}/ST=${STATE}/L=${CITY}/O=${ORGANIZATION}/OU=${ORG_UNIT}/CN=${COMMON_NAME}/emailAddress=${EMAIL}"
+
+  openssl req -new -newkey rsa:2048 -days 365 -nodes -sha256 -x509 -keyout "${KEYDIR}/rabbit-mq-${SUBDOMAIN_TOKEN}.key" -out "${KEYDIR}/rabbit-mq-${SUBDOMAIN_TOKEN}.crt" -subj "${SUBJECT}" -reqexts SAN -extensions SAN -config <(cat /etc/ssl/openssl.cnf <(printf "\n[SAN]\nsubjectAltName=${ALT_NAMES}\n")) > /dev/null
+
+  echo "SSL certificate for Rabbitcreated and stored at ${KEYDIR}/rabbit-mq-${SUBDOMAIN_TOKEN}.crt, private key stored at ${KEYDIR}/rabbit-mq-${SUBDOMAIN_TOKEN}.key."
+  echo "The certificate is a wildcard for the following domains: ${ALT_NAMES}"
+
 }
 
 ssl_requests () {
