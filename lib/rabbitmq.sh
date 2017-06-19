735af2798e154ce2c7e41f42890c0b2a12b567fa
Chuck D'Antonio <c_dantonio@acm.org>
2017-06-19 16:55:59 -0400

    Corrected some sloppiness

--- lib/rabbitmq.sh
+++ lib/rabbitmq.sh
@@ -3,6 +3,6 @@ set_rabbit_config () {
   RABBIT_PRIVATE_KEY=`cat ${KEYDIR}/rabbit-mq-${SUBDOMAIN_TOKEN}.key | perl -pe 's#\n#\x5c\x5c\x6e#g'`
   RABBIT_CERTIFICATE=`cat ${KEYDIR}/rabbit-mq-${SUBDOMAIN_TOKEN}.crt | perl -pe 's#\n#\x5c\x5c\x6e#g'`
 
-  rabbit_json=`export ROUTER_PRIVATE_KEY ROUTER_CERTIFICATE TCP_ROUTER_PORTS; envsubst < api-calls/rabbitmq/networking.json ; unset ROUTER_PRIVATE_KEY ROUTER_CERTIFICATE TCP_ROUTER_PORTS`
-  set_properties "${PCF_OPSMAN_SLUG}" "${networking_json}"
+  rabbit_json=`export RABBIT_PRIVATE_KEY RABBIT_CERTIFICATE ; envsubst < api-calls/rabbitmq/networking.json ; unset RABBIT_PRIVATE_KEY RABBIT_CERTIFICATE TCP_ROUTER_PORTS`
+  set_properties "${PCF_OPSMAN_SLUG}" "${rabbit_json}"
 }
